407c81a3f56a3b682bd5a0bddfe246e674639a1e
Endale2 <endale406@gmail.com>
2024-09-04 20:59:35 +0300

    updated

--- routes/rssRoutes.js
+++ routes/rssRoutes.js
@@ -8,7 +8,7 @@ router.post('/rss/add',authMiddleware,  addRSSFeed);
 router.delete('/rss/:id',authMiddleware,  deleteRSSFeed);
 router.post('/rss/reload',authMiddleware,  reloadArticles);
 router.get('/rss/check-daily-scan',authMiddleware,checkDailyScanStatus);
-router.delete('/rss/delete-daily-scan',authMiddleware, deleteTodaysDailyScan);
+router.post('/rss/delete-daily-scan',authMiddleware, deleteTodaysDailyScan);
 
 
 export default router;
